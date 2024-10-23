05e2e9a6a306b2c9b1228cc1a3dfa8e157ee45ee
Martin, Corinna <corinnamartin@my.unt.edu>
2024-10-23 18:13:31 +0000

    Update main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+
+    //Interactive mode
+    if(argc == 1){
+
+        //Until user exits shell
+        while(1 == 1){
+            
+            //Get user input
+
+            //If user input contains multiple commands, separate at ';' and execute sequentially
+
+        }
+    }
+
+    //Batch mode
+    if(argc == 2){
+
+    }
+
+    return 0;
+}
