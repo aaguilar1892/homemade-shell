96604c810f83860ee8251e2d90e7e4984b39f858
Martin, Corinna <corinnamartin@my.unt.edu>
2024-11-11 18:53:43 +0000

    Add new file

---
+++ preprocessing.h
@@ -0,0 +1,10 @@
+//preprocessing.h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+char* preprocessing(char* cmd); //Removes extra whitespace from issued command
