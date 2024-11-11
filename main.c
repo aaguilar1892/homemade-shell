c2b52935c22c5ab66ce739787669020041167818
Martin, Corinna <corinnamartin@my.unt.edu>
2024-11-11 18:52:40 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 #include "myhistory.h"
+#include "preprocessing.h"
 #include "cd.c"
 
 int main(int argc, char* argv[]){
@@ -25,37 +26,8 @@ int main(int argc, char* argv[]){
             fgets(cmd, 512, stdin);
             cmd[strlen(cmd)-1] = '\0';
 
-            //Delete leading whitespace
-            for(int i = 0; i < strlen(cmd); ++i){
-                    if(!isspace(cmd[i])){
-                            strncpy(cmd, cmd + i, strlen(cmd));
-                            break;
-                    }
-            }
-
-            //Delete trailing whitespace
-            for(int i = strlen(cmd) - 1; i >= 0 && isspace(cmd[i]); --i){
-                   cmd[i] = '\0';
-            }
-
-            //Delete extra whitespace in the middle...
-            bool currSpace = false;
-            bool nextSpace = false;
-
-            for(int i = 0; i < strlen(cmd); ++i){
-
-                    currSpace = isspace(cmd[i]);
-                    nextSpace = isspace(cmd[i+1]);
-
-                    if(currSpace){
-                        if(nextSpace){
-                            for(int j = i; j < strlen(cmd); ++j){
-                                    cmd[j] = cmd[j+1];
-                            }
-                            --i;
-                        }
-                    }
-            }
+            //Remove extra white space from command
+            strcpy(cmd, preprocessing(cmd));
 
             //If empty command line, process next command
             if(strcmp(cmd, "\0") == 0 || strncmp(cmd, " ", 1) == 0 || strcmp(cmd, ";") == 0){
