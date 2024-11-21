6ae798f82bcb3bf9147b2549982b6f600420cf2d
Nguyen, Elisabeth <elisabethnguyen@my.unt.edu>
2024-11-21 01:08:18 +0000

    Update main.c add alias and cd functionality

--- main.c
+++ main.c
@@ -36,6 +36,8 @@ int main(int argc, char* argv[]){
 
             //If user input contains multiple commands, separate at ';' and execute sequentially
 
+            //Check if command is an alias
+            checkAlias(cmd);
             //Execute custom commands
             if(strncmp(cmd, "myhistory", 9) == 0){
                 //Execute myhistory commands
@@ -54,6 +56,56 @@ int main(int argc, char* argv[]){
 
                 //Usage statements
                 if(strncmp(cmd, "myhistory -e", 12) == 0 && !isdigit(cmd[13])) {printf("Usage: myhistory -e <myhistory_number>\n");}
+            }
+            //Alias command
+            else if(strncmp(cmd, "alias", 5) == 0) {
+                //test
+                printf("reached alias\n");
+
+                char alName[503]; //char array for input (length enough for max name and command length in form name[100]='command[400]') 
+                //if no input, print existing aliases
+                if ((strcmp(cmd, "alias")==0)||(strcmp(cmd,"alias;")==0)) {
+                    //test
+                    printf("printing aliases\n");
+                    printAl();
+                }
+                //if -c flag set, clear all aliases
+                else if ((strcmp(cmd, "alias -c")==0)||(strcmp(cmd,"alias -c;") == 0)) {
+                    //test 
+                    printf("alias clearing (not remove)\n");
+                    aliasClear();
+                }
+                //if -r flag set, remove specific alias
+                else if (strncmp(cmd,"alias -r ",9)==0) {
+                    //test
+                    printf("removing alias\n");
+                    
+                    strcpy(alName, cmd+9);
+                    printf("%s\n", alName);
+                    aliasDel(alName);
+
+                }
+                //add alias
+                else if (strncmp(cmd, "alias ", 6) == 0) {
+                    //test
+                    printf("adding alias\n");
+                    
+                    strcpy(alName, cmd+6);
+                    printf("%s\n", alName);
+                    aliasAdd(alName);
+                }
+            }
+            //cd command
+            else if(strncmp(cmd, "cd",2)==0) {
+                if ((strcmp(cmd,"cd")==0)||(strcmp(cmd,"cd;")==0)) {
+                    cd(NULL);
+                }
+                else {
+                    char path[512];
+                    strcpy(path, cmd+3);
+                    printf("%s\n",path);
+                    cd(path);
+                }
             }
             else{
                 //Execute other commands
