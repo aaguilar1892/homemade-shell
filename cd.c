869e6e47fbdd9f26ca5ba2bc8c132a62d6a6a71b
Nguyen, Elisabeth <elisabethnguyen@my.unt.edu>
2024-11-21 01:05:49 +0000

    Update cd.c

--- cd.c
+++ cd.c
@@ -1,23 +1,23 @@
-//Elisabeth Nguyen
-#include "cd.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 void cd(char* newd) {
      if (newd == NULL) {
+        //if path not provided, change directory to home
         char* user_home = getenv("HOME");
         chdir(user_home);
-         printf("changed to home\n");
     }
     else {
+        //if path provided, change directory
         chdir(newd);
-        printf("changed to %s\n", newd);
-        
-        char cwd[300];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("Current working dir: %s\n", cwd);
-        } else {
-            perror("getcwd() error");
-        }
+    }
+    //print current working directory
+    printCwd();
+}
+void printCwd() {
+    char cwd[300];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("Current working dir: %s\n", cwd);
+    } 
+    
+    else {
+        perror("getcwd() error");
     }
 }
