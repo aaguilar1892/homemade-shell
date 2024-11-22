cb874b79c467888886b1e7d79ecfbe9c14c6fe2a
Nguyen, Elisabeth <elisabethnguyen@my.unt.edu>
2024-11-22 20:16:05 +0000

    Update cd.c

--- cd.c
+++ cd.c
@@ -1,3 +1,12 @@
+/*Name: Elisabeth Nguyen
+ *Command: cd (Change Directory)
+ *Description:  cd() takes string input, changes directory to provided path or to the user's home if path not provided, and calls printCwd()
+ *              printCwd() prints the current working directory
+*/
+#include "cd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 void cd(char* newd) {
      if (newd == NULL) {
         //if path not provided, change directory to home
